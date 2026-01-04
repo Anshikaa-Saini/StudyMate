db35b2bc12bcc458ce90d9ab75d258420e22ce19
Anshika Saini <143860130+Anshikaa-Saini@users.noreply.github.com>
2026-01-04 17:17:39 +0000

    document api

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 
 import {
-    uploadDocuments,
+    uploadDocument,
     getDocuments,
     getDocument,
     deleteDocument,
-    updateDocument,
 } from '../controllers/documentController.js';
-import { protect } from '../middleware/auth.js';
+import protect from '../middleware/auth.js';
 import upload from '../config/multer.js';
 
 const router = express.Router();
@@ -15,10 +14,9 @@ const router = express.Router();
 //All routes are protected
 router.use(protect);
 
-router.post('/upload', upload.single('file'), uploadDocuments);
+router.post('/upload', upload.single('file'), uploadDocument);
 router.get('/', getDocuments);
 router.get('/:id', getDocument);
 router.delete('/:id', deleteDocument);
-router.put('/:id', updateDocument);
 
 export default router; 
